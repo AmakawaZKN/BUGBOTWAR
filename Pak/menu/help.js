76cd131fe40284bdb90435d5accb66dcad27c0c5
AmakawaZKN <lynxxnotcounter@gmail.com>
2022-08-20 19:38:25 +0700

    zykuan

---
+++ Pak/menu/help.js
@@ -0,0 +1,38 @@
+/*  TQ TO JANGAN DI HAPUS, KALO NAMBAHIN NAMA LU GPP LAH*/
+
+const help = (prefix,shp,monoscape) => {
+	return `┏━━⬣  WARR ⚡
+┃〽️ ️${prefix}troliv2${monoscape}
+┃〽️ ️${prefix}buglist${monoscape}
+┃〽️ ${prefix}vweb${monoscape}
+┃〽️ ️${prefix}virgam${monoscape}
+┃〽️ ️${prefix}virtag${monoscape}
+┃〽️ ️${prefix}virtex${monoscape}
+┃〽️ ️${prefix}bugsw${monoscape}
+┃〽️ ${prefix}bugpdf${monoscape}
+┃〽️ ${prefix}bugpc${monoscape}
+┃〽️ ${prefix}bugbutton${monoscape}
+┃〽️ ${prefix}bugkatalog${monoscape}
+┃〽️ ${prefix}bugimg${monoscape}
+┃〽️ ${prefix}buggif${monoscape}
+┃〽️ ${prefix}bugdoc${monoscape}
+┃〽️ ${prefix}bugtroli${monoscape}
+┃〽️ ${prefix}bugloc${monoscape}
+┃〽️ ${prefix}bugvn${monoscape}
+┃〽️ ${prefix}bugstik${monoscape}
+┃〽️ ${prefix}bugkontak${monoscape}
+┃〽️ ${prefix}buggc [ Jumlah ]${monoscape}
+┃〽️ ${prefix}sendbug${monoscape}
+┗━━⬣  ༱ 𝑴𝑹.𝐕✗𝐨 ༱
+
+┏━━⬣  Thanks To 〽️
+┃ 🔥 MhankBarBar
+┃ 🔥 Diri Sendiri
+┃ 🔥 Orang Tua
+┃ 🔥 Tuhan Yang Maha Esa
+┃ 🔥 Pak Tzy
+┃ 🔥 Dan Penyedia node_modules
+┗━━⬣  ༱ 𝑴𝑹.𝐕✗𝐨 ༱`
+}
+
+exports.help = help
